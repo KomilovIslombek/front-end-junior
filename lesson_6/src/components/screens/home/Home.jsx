90a1bd9b341f1e332b65375d68df3804ee5856c6
KomilovIslombek <islambek445@gmail.com>
2024-06-04 13:36:03 +0500

    lesson_6

---
+++ lesson_6/src/components/screens/home/Home.jsx
@@ -0,0 +1,13 @@
+import Layout from '../../layout/Layout'
+
+const Home = () => {
+	return (
+		<>
+			<Layout bgImage='/images/home-bg-ik.jpg'>
+				<h1>Hi! react</h1>
+			</Layout>
+		</>
+	)
+}
+
+export default Home
